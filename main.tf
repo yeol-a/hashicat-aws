557b3a2eb33ad023de785a04417c1fbe4b215dd9
yeol-a <kay061476@gmail.com>
2023-03-03 04:51:12 +0000

    Added modified tags

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "devops",
-    Billable = true 
+    "Department" = "devops",
+    "Billable" = true 
   }
 }
 
